5492920c725194751c8a21ebae6fbb5f7731dd0e
나경호 <gangho324@gmail.com>
2024-04-01 16:11:44 +0900

    project commit

---
+++ src/main/java/com/loghabit/loghabitbe/LogHabitBeApplication.java
@@ -0,0 +1,13 @@
+package com.loghabit.loghabitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHabitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogHabitBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loghabit/loghabitbe/LogHabitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loghabit.loghabitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHabitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
